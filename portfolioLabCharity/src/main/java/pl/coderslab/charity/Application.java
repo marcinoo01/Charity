bf88f8415bfa63b48ec8606121fe4d27539d6476
marcinoo01 <85891362+marcinoo01@users.noreply.github.com>
2021-10-06 21:32:12 +0200

    whole form(donation) without post method

--- portfolioLabCharity/src/main/java/pl/coderslab/charity/Application.java
+++ portfolioLabCharity/src/main/java/pl/coderslab/charity/Application.java
@@ -3,8 +3,11 @@ package pl.coderslab.charity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pl.coderslab.charity.repository.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "pl.coderslab.charity")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run((Application.class), args);
--- portfolioLabCharity/src/main/java/pl/coderslab/charity/controller/FundationOrganizationController.java
+++ portfolioLabCharity/src/main/java/pl/coderslab/charity/controller/FundationOrganizationController.java
@@ -3,20 +3,39 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import javax.validation.Valid;
 
 @Controller
 public class FundationOrganizationController {
 
     private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public FundationOrganizationController(CategoryRepository categoryRepository) {
+    public FundationOrganizationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping("/foundation")
-    public String foundation(Model model){
+    public String foundation(Model model) {
         model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("foundations", institutionRepository.findAll());
+        model.addAttribute("donation", new Donation());
         return "foundation/selectCategory";
+    }
+
+    @PostMapping("/foundation")
+    public String foundation(Donation donation){
+        donationRepository.save(donation);
+        return "/";
     }
 }
